8f3a236b186a3fe682108d24ae264a37782b38d5
ROUKAYA SAIED <rsaied@dom.tti>
2019-02-04 16:19:52 +0100

    posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Post } from './Post';
+
 
 @Component({
   selector: 'app-root',
@@ -8,11 +8,6 @@ import { Post } from './Post';
 })
 export class AppComponent {
   title = 'Blog';
+   
   
-   POSTS: Post[] = [
-  { title: 'Mon premier post', content: 'bla bla bla', loveIts: 0, created_at: new Date },
-  { title: 'Mon deuxième post', content: 'bla bla bla', loveIts: 0, created_at: new Date },
-  { title: 'Encore un post', content: 'bla bla bla', loveIts: 0, created_at: new Date }
-]
-
 }
